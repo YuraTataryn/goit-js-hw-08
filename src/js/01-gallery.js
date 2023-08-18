991cfe7ecf18d65e2ac51ee4a9c87e556eb5f445
YuraTataryn <127976814+YuraTataryn@users.noreply.github.com>
2023-08-18 07:33:16 -0700

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from "./gallery-items.js";
+
+const galleryListItem = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      width="250px"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+galleryListItem.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery__link");
