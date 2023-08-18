9d257c7b520eee734360e0202c8b244462adc02c
YuraTataryn <127976814+YuraTataryn@users.noreply.github.com>
2023-08-18 07:46:52 -0700

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const vimeoPlayer = new Player('vimeo-player');
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+}
+const updateSavedTime = throttle(async () => {
+  const currentTime = await vimeoPlayer.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+vimeoPlayer.on('timeupdate', updateSavedTime);
